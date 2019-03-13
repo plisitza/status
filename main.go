b74f5a5ba68a236d8d6b9b5b86e0e4c26713c9c0
phil <phil.lisitza@twopoint.com>
2019-03-13 11:22:00 -0500

    just musings

--- main.go
+++ main.go
@@ -6,6 +6,26 @@ import "fmt"
 // or display itself via command line-style lines of logging, or something like https://github.com/gizak/termui
 // for providing info to a more gui display layer, start a goroutine that listens on a configured port
 
+const (
+	Terminal int = iota
+	Gui
+	Print
+)
+
+type Configuration struct {
+}
+
+type statusEntry struct {
+	statusName string
+	statusType
+}
+
+type globals struct {
+	statusInfo map[string]statusEntry
+	statusInC  chan statusEntry
+}
+
 func main() {
 	fmt.Println("hi!")
+	globals.statusInfo = make(map[string]statusEntry) // map of name to abritrary structure
 }
